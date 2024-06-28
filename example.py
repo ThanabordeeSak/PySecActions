799c78cb30c353e93d0235228cd24031907f55d8
ThanabordeeSak <135211645+ThanabordeeSak@users.noreply.github.com>
2024-06-28 14:08:37 +0700

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
 import os
 
-def insecure_function():
-  password = "hardcode password"
-  os.system(f"echo {password}")
+# def insecure_function():
+#   password = "hardcode password"
+#   os.system(f"echo {password}")
 
 def secure_function():
   password = os.getenv("PASSWORD")
